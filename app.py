e31c6a08b81b5f7471141bd0c906a242b7c0c069
cameronmoodley <moodley.cameron@gmail.com>
2020-09-23 10:34:46 +0200

    added in databases configurations

--- app.py
+++ app.py
@@ -18,6 +18,33 @@ db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
 
+# Product Class/Model
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self,  name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
+
+# Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'qty')
+
+
+# Init Schema
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
 
 # Run Server
 if __name__ == '__main__':
